1acf3d76d1abd2d7675229584a86cfd8a5531e14
yourcrazyuncle <yourcrazyuncle@protonmail.com>
2026-04-03 08:28:26 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log(`hello`);
+console.log(`this is the concole.log`);
+
+window.alert(`Hey Bean!`);
+window.alert(`Thanks for checking out my website!`);
+window.alert("I love you. Namarie.");
