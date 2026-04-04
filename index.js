54535c10660d5c542fed34c73952edf5fbb8ac28
JF-Lovely <yourcrazyuncle@protonmail.com>
2026-04-03 19:25:42 -0700

    Hey Github, fuck you.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 console.log(`hello`);
 console.log(`this is the concole.log`);
 
-window.alert(`Hey Bean!`);
-window.alert(`Thanks for checking out my website!`);
-window.alert("I love you. Namarie.");
+window.alert(`Welcome to GitHub. Fuck you.`)
